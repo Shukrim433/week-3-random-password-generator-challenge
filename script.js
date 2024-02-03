23b9be21541f257248f690a63b22143c183c395c
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-02-03 15:36:34 +0000

    created user preferences for the password characters using the 'confirm()' function

--- script.js
+++ script.js
@@ -16,6 +16,18 @@ var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 var lowercase = 'abcdefghijklmnopqrstuvwxyz'
 var specialCharacters = '!"#$%&()*+,-./:;<>=?@[\]^_`{}~|'
 var numbers = '1234567890'
+
+//set out the user preferences
+
+//preference for the password characters
+var lowerCasePref = confirm('Do you want lowercase letters in your password?')
+var uppercasePref = confirm('Do you want uppercase letters in your password?')
+var numbersPref = confirm('Do you want numbers letters in your password?')
+var specialCharactersPref = confirm('Do you want uppercase letters in your password?')
+
+//preference for password length
+
+
 }
 
 // Add event listener to generate button
